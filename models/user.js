94dc0b0fb6a066cc8357f18e251e13949d5c2571
SagarMedtiya <sagarmedtiya31@gmail.com>
2022-04-08 16:39:21 +0530

    add user model file

---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+import { APP_URL } from '../config'
+
+const userSchema = new Schema({
+    name: { type: String, required:true },
+    email: { type: String, required:true, unique: true },
+    password: { type: String, required:true },
+    role: { type: String, default: 'customer' },
+},{timestamps:true,toJSON:{ getters:true }});
+
+
+
+export default mongoose.model('User', userSchema, 'users');
