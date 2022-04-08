0c877cc7726ae39d14c1814c2b1fe5503d39729a
SagarMedtiya <sagarmedtiya31@gmail.com>
2022-04-08 16:32:28 +0530

    Add server file

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 import express from 'express'
-import { PORT } from './config';
+import { PORT, MONGODB } from './config';
 import routes from './routes/index'
 import error  from './middlewares/error'
+import mongoose from 'mongoose';
+import path from 'path';
 const app = express();
 
+
+
+ mongoose.connect( MONGODB,{ useNewUrlParser: true, useUnifiedTopology: true
+    }).then(()=>{
+        console.log('Connection Successful');
+    }).catch((error)=>{     
+        console.log('Something went wrong', error)
+    });
+
+    //mongoose connection doni
 app.use(express.json())
 app.use('/api',routes);
+global.appRoot = path.resolve(__dirname);
 
+app.use(express.urlencoded({ extended:false }))
 app.use(error);
+app.use('/uploads',express.static('uploads'));
 app.listen(PORT,()=>{
     console.log(`Listening to port ${PORT} `)
 })
