2a8a0d9176f90f9ee891a736a668d61c9bfb362c
SagarMedtiya <sagarmedtiya31@gmail.com>
2022-04-08 16:39:31 +0530

    add index route file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
-
+import { auth } from '../middlewares';
 const router = express.Router();
-import { registerController } from "../controllers";
-
+import { registerController, loginController, userController,refreshController, productController } from "../controllers";
+import { admin } from '../middlewares'
 router.post('/register',registerController.register);
+router.post('/login',loginController.login);
+router.post('/logout',auth,loginController.logout);
+router.get('/me',auth,userController.me);
+router.post('/refresh',refreshController.refresh);
+router.post('/product',[auth, admin],productController.create);
+router.put('/product/:id',[auth, admin],productController.update);  //updating the products
+router.delete('/product/:id',[auth, admin],productController.delete); //deleting the products
+router.get('/product',productController.index);  //get all the products details
+router.get('/product/:id',productController.show);  //get all the products details
 
 export default router;
